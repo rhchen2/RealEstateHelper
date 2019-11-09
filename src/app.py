cc51f67d717d1bbaf86063e47d115ae51ed10760
Ian Renfro <ian@renfro.org>
2019-11-09 11:18:45 -0800

    Fixed dir structure

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-from services.zillow.GetSearchResults import getSearchResults
-from classes.SearchResultClass import SearchResultClass
+from main.services.zillow.GetSearchResults import getSearchResults
+from main.classes.SearchResultClass import SearchResultClass
 def run():
     myParams ={
         "zws-id":"X1-ZWz17jvxcowwln_7lwvq",
@@ -9,3 +9,4 @@ def run():
 
     searchResult = SearchResultClass("X1-ZWz17jvxcowwln_7lwvq","60611","420 E Ohio St")
     searchResult.getSearchResultsHelper()
+    print("done")
