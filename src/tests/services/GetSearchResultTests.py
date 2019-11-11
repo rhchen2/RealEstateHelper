a8af9391bc7928c4a7fbda734d91d90155fdcc07
ryanchen <ryanchen1996@yahoo.com>
2019-11-11 10:37:58 -0600

    fixed imports

--- src/tests/services/GetSearchResultTests.py
+++ src/tests/services/GetSearchResultTests.py
@@ -1,13 +1,13 @@
 import unittest
 
-from main.classes.SearchResultClass import SearchResultClass
+import src.main.classes.SearchResultClass
 
 def getResultsTests():
     zwsid = "X1-ZWz17jvxcowwln_7lwvq",
     citystatezip = "60611",
     address = "420 E Ohio St"
 
-    searchResult = SearchResultClass(zwsid,citystatezip,address)
+    searchResult = src.SearchResultClass(zwsid, citystatezip, address)
     searchResult.getSearchResultsHelper()
 
     assert searchResult.results[0].zpid == 2097525662
